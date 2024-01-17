7327c48bcb2049253e9bbf1672cf49981398815b
subhash <ksubhash8055@gmail.com>
2024-01-18 00:34:40 +0530

    final commit

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:4000';
+const API_URL = 'https://blog-post-backend-ytdu.onrender.com';
 const axiosInstance = axios.create({
     baseURL: API_URL,
     timeout: 10000,
